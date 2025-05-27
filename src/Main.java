3b9fbd7429940ab6ffdb79ef3364e4e0e49ffd6e
sermen8878 <sermen88@yandex.ru>
2025-05-27 21:50:13 +0500

    creating a class structure for an online store

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,132 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+abstract class Product {
+    private final String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public abstract double getPrice();
+    public abstract boolean isSpecial();
+
+    @Override
+    public abstract String toString();
+}
+
+class SimpleProduct extends Product {
+    private final double price;
+
+    public SimpleProduct(String name, double price) {
+        super(name);
+        this.price = price;
+    }
+
+    @Override
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "%s: %.2f", getName(), getPrice());
+    }
+}
+
+class DiscountedProduct extends Product {
+    private final double basePrice;
+    private final int discount;
+
+    public DiscountedProduct(String name, double basePrice, int discount) {
+        super(name);
+        this.basePrice = basePrice;
+        this.discount = discount;
+    }
+
+    @Override
+    public double getPrice() {
+        return basePrice * (100 - discount) / 100;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "%s: %.2f (%d%% скидка)", getName(), getPrice(), discount);
+    }
+}
+
+class FixPriceProduct extends Product {
+    private static final double FIXED_PRICE = 99.99;
+
+    public FixPriceProduct(String name) {
+        super(name);
+    }
+
+    @Override
+    public double getPrice() {
+        return FIXED_PRICE;
+    }
+
+    @Override
+    public boolean isSpecial() {
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(Locale.US, "%s: Фиксированная цена %.2f", getName(), FIXED_PRICE);
+    }
+}
+
+class ShoppingCart {
+    private final List<Product> products = new ArrayList<>();
+
+    public void addProduct(Product product) {
+        products.add(product);
+    }
+
+    public void printContents() {
+        double total = 0;
+        int specialCount = 0;
+
+        for (Product product : products) {
+            System.out.println(product);
+            total += product.getPrice();
+            if (product.isSpecial()) {
+                specialCount++;
+            }
+        }
+
+        System.out.printf(Locale.US, "Итого: %.2f%n", total);
+        System.out.println("Специальных товаров: " + specialCount);
+    }
+}
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        ShoppingCart cart = new ShoppingCart();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        cart.addProduct(new SimpleProduct("Хлеб", 30.0));
+        cart.addProduct(new DiscountedProduct("Молоко", 50.0, 20));
+        cart.addProduct(new FixPriceProduct("Книга"));
+        cart.addProduct(new DiscountedProduct("Смартфон", 1000.0, 15));
+        cart.addProduct(new FixPriceProduct("Блокнот"));
+
+        cart.printContents();
     }
 }
